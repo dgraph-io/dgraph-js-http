f995ea45cb7b7aa55511ff94c6d6ec4998ddf317
Martin Martinez Rivera <martinmr@dgraph.io>
2019-02-15 17:27:21 -0800

    Fix Travis scripts.

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -7,7 +7,7 @@ fi
 
 function quit {
     echo "Shutting down Dgraph alpha and zero."
-    curl -s localhost:8080/admin/shutdown
+    curl -s localhost:8180/admin/shutdown
     # Kill Dgraph zero.
     kill -9 $(pgrep -f "dgraph zero") > /dev/null
 
@@ -26,7 +26,7 @@ function quit {
 
 function start {
     echo -e "Starting Dgraph alpha."
-    dgraph alpha -p data/p -w data/w --lru_mb 4096 --zero localhost:5080 > data/server.log 2>&1 &
+    dgraph alpha -p data/p -w data/w -o 100 --lru_mb 4096 --zero localhost:5080 > data/server.log 2>&1 &
     # Wait for membership sync to happen.
     sleep $sleepTime
     return 0
