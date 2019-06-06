c6d62f88ad565a99fc629ee7a090e8f38cc0f527
Paul Korzhyk <paul@dgraph.io>
2019-06-06 17:13:01 +0300

    Change to AMD modules, clean up build folder, bump version

--- lib/util.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function isAbortedError(error: any): boolean;
-export declare function isConflictError(error: any): boolean;
-export declare function stringifyMessage(msg: object): string;
