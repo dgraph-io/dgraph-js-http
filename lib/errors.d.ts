192e20f2695c74f7f1bec960eb145db1aaac1cd6
Ahmed El Bannan <akelbann@gmail.com>
2019-12-18 01:06:25 -0700

    Build after merge

--- lib/errors.d.ts
+++ lib/errors.d.ts
@@ -1,6 +1,7 @@
 export declare const ERR_NO_CLIENTS: Error;
 export declare const ERR_FINISHED: Error;
 export declare const ERR_ABORTED: Error;
+export declare const ERR_BEST_EFFORT_REQUIRED_READ_ONLY: Error;
 export declare class CustomError extends Error {
     readonly name: string;
     constructor(message?: string);
